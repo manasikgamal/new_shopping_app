3e1e715a864ce5406789eb7a385753b114e82e18
manasikgamal <32040755+manasikgamal@users.noreply.github.com>
2023-10-06 11:26:46 +0300

    Update currencyservices.ts

--- src/components/services/currencyservices.ts
+++ src/components/services/currencyservices.ts
@@ -2,23 +2,13 @@ import { Dispatch } from "@reduxjs/toolkit";
 import axios from "axios";
 import { ActionTypes } from "../actions/types";
 
-export const currencySymbol = (currency_name: string) => (dispatch: Dispatch) => {
-    var currency_symbols: { [key: string]: string } = {
-        'USD': '$', // US Dollar
-        'GBP': '£', // British Pound Sterling
-        'JPY': '¥', // Japanese Yen
-        'RUB': '₽',
-        'AUD': '$'
-    };
-    if (currency_symbols[currency_name] !== undefined) {
+export const currencySymbol = (currency_name: cstate) => (dispatch: Dispatch) => {
         dispatch({
             type: ActionTypes.currencySymbolAction,
-            payload: { symbol: currency_symbols[currency_name], name: currency_name }
+            payload: currency_name
         })
         localStorage.setItem("currency", JSON.stringify(currency_name));
-    }
 }
-
 export const getCurrencies = () => async (dispatch: Dispatch) => {
     return await axios({
         url: 'http://localhost:4000/',
@@ -26,7 +16,10 @@ export const getCurrencies = () => async (dispatch: Dispatch) => {
         data: {
             query: `
           query getcurrencies{
-            currencies
+            currencies{
+                label
+                symbol
+              }
          }`
         }
     }).then((result) => {
@@ -37,16 +30,16 @@ export const getCurrencies = () => async (dispatch: Dispatch) => {
     })
 }
 
-export const currencylocalSymbol = (currency_name: string): string => {
-    var currency_symbols: { [key: string]: string } = {
-        'USD': '$', // US Dollar
-        'GBP': '£', // British Pound Sterling
-        'JPY': '¥', // Japanese Yen
-        'RUB': '₽',
-        'AUD': '$'
-    };
-    if (currency_symbols[currency_name] !== undefined) {
-        return currency_symbols[currency_name]
-    }
-    return 'no'
-}
+//export const currencylocalSymbol = (currency_name: string): string => {
+//     var currency_symbols: { [key: string]: string } = {
+//         'USD': '$', // US Dollar
+//         'GBP': '£', // British Pound Sterling
+//         'JPY': '¥', // Japanese Yen
+//         'RUB': '₽',
+//         'AUD': '$'
+//     };
+//     if (currency_symbols[currency_name] !== undefined) {
+//         return currency_symbols[currency_name]
+//     }
+//     return 'no'
+// }
